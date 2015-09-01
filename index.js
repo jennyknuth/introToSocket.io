2e09c2bb12ab786c6141e4d8eec0cccce5de6327
Jenny Knuth <inkboco@gmail.com>
2015-09-01 15:49:37 -0600

    basic chat app without persistence

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 //Your code BELOW
+io.on('connection', function (socket) {
+  console.log('connected');
+  socket.on('disconnect', function(){
+    console.log('disconnected');
+  })
+})
 
+io.on('connection', function (socket) { //socket io server side code
+  socket.on('chat message', function(msg) {
+    console.log(msg);
+    io.emit('chat message', msg)
+  })
+})
 
 
 http.listen(3000, function(){
